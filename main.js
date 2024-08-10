10c3428de3fd14a1df9ff0dfc2ffcdb972a2abd2
nmogoko <nancymogoko@gmail.com>
2024-08-10 23:39:20 +0100

    Added close modal functionality

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 const readingList = document.getElementById("reading-list");
 const readingButton = document.getElementById("rl");
 const modal = document.getElementById("modal-info");
+const saveButton = document.getElementById("save-button");
+const closeButton = document.getElementById("close-button");
+const close = document.getElementsByClassName("btn-close");
 
-
-readingButton.addEventListener("click", function displayReadingList() {
-    modal.style.display = "block";
+function closeModal (){
+    modal.style.display = "none";
 }
-)
+
+closeButton.addEventListener("click", closeModal);
+// closeButton.addEventListener("click", ()=>{
+//     this.style.display = "none";
+// <Why will this not work? })
+
+for (let i = 0; i < close.length; i++) {
+    close[i].addEventListener("click", closeModal);
+  }
+
+
+readingButton.addEventListener("click", ()=>{
+    modal.style.display = "block";
+});
